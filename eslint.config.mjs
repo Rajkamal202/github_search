0b5c1a53ba147d851aa6aacd6adc2c6bf2ebd2fd
Rajkamal Rao <122449211+Rajkamal202@users.noreply.github.com>
2025-01-08 00:08:04 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,4 +13,12 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
+module.exports = {
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "react/no-unescaped-entities": "off",
+    "@next/next/no-img-element": "off"
+  }
+};
+
 export default eslintConfig;
