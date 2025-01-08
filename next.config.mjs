ae9aea9de42b5b36990651b1bbe98b8800b4d073
Rajkamal Rao <122449211+Rajkamal202@users.noreply.github.com>
2025-01-08 11:20:37 +0530

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
