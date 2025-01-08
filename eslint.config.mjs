4cf0fa34a2ba9930972e11ab38abb48b1d3978f0
Rajkamal Rao <122449211+Rajkamal202@users.noreply.github.com>
2025-01-08 09:10:08 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,9 +11,14 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable specific ESLint rules
+      "@typescript-eslint/no-explicit-any": "off",
+      "react/no-unescaped-entities": "off",
+    },
+  },
 ];
 
-
-
-
 export default eslintConfig;
+
