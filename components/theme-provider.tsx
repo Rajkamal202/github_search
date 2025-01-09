3d36aab2a31fe67fd28cf22f34c8ba187b7bd505
Rajkamal Rao <122449211+Rajkamal202@users.noreply.github.com>
2025-01-09 08:25:14 +0530

    Update theme-provider.tsx

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,9 +2,9 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes"
+import type { ComponentProps } from "react"
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+export function ThemeProvider({ children, ...props }: ComponentProps<typeof NextThemesProvider>) {
   const [mounted, setMounted] = React.useState(false)
 
   React.useEffect(() => {
@@ -17,4 +17,5 @@ export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
 
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
+
 
